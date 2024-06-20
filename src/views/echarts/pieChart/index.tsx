a06828b4adaeda8df7452062b490af07c2e04caf
zhengcong <zhengcong@linno.cn>
2024-06-20 14:21:08 +0800

    feat: 🚀 新增柱形、折线、饼图等echarts完结

--- src/views/echarts/pieChart/index.tsx
+++ src/views/echarts/pieChart/index.tsx
@@ -1,11 +1,97 @@
+import { useEffect, useRef } from "react";
+import * as echarts from "echarts";
+import useEcharts from "@/hooks/useEcharts";
 import "./index.less";
 
 const PieChart = () => {
-	return (
-		<div className="content-box">
-			<span className="text">PieChart 🍓🍇🍈🍉</span>
-		</div>
-	);
+	const echartsRef = useRef<HTMLDivElement>(null);
+	let option: echarts.EChartsOption = {
+		tooltip: {
+			trigger: "item",
+			formatter: "{a} <br/> {b} : {c} ({d}%)",
+		},
+		legend: {
+			left: "center",
+			top: "bottom",
+			data: ["rose1", "rose2", "rose3", "rose4", "rose5", "rose6", "rose7", "rose8"],
+			textStyle: {
+				color: "#a1a1a1",
+			},
+		},
+		toolbox: {
+			show: true,
+			feature: {
+				mark: { show: true },
+				dataView: { show: true, readOnly: false },
+				restore: { show: true },
+				saveAsImage: { show: true },
+			},
+		},
+		series: [
+			{
+				name: "Radius Mode",
+				type: "pie",
+				radius: [60, 280],
+				center: ["50%", "50%"],
+				roseType: "radius",
+				itemStyle: {
+					borderRadius: 5,
+				},
+				label: {
+					show: true,
+				},
+				emphasis: {
+					label: {
+						show: true,
+					},
+				},
+				data: [
+					{
+						value: 40,
+						name: "rose1",
+					},
+					{
+						value: 33,
+						name: "rose2",
+					},
+					{
+						value: 28,
+						name: "rose3",
+					},
+					{
+						value: 22,
+						name: "rose4",
+					},
+					{
+						value: 20,
+						name: "rose5",
+					},
+					{
+						value: 15,
+						name: "rose6",
+					},
+					{
+						value: 12,
+						name: "rose7",
+					},
+					{
+						value: 10,
+						name: "rose8",
+					},
+				],
+			},
+		],
+	};
+
+	useEffect(() => {
+		const myChart: echarts.EChartsType = echarts.init(echartsRef.current as HTMLDivElement);
+		const { deleteEvent } = useEcharts(myChart, option);
+		return () => {
+			myChart && myChart.dispose();
+			deleteEvent && deleteEvent();
+		};
+	}, []);
+	return <div ref={echartsRef} className="content-box"></div>;
 };
 
 export default PieChart;
--- src/views/echarts/radarChart/index.tsx
+++ src/views/echarts/radarChart/index.tsx
@@ -1,11 +1,77 @@
+import { useEffect, useRef } from "react";
+import * as echarts from "echarts";
+import useEcharts from "@/hooks/useEcharts";
 import "./index.less";
 
 const RadarChart = () => {
-	return (
-		<div className="content-box">
-			<span className="text">RadarChart 🍓🍇🍈🍉</span>
-		</div>
-	);
+	const echartsRef = useRef<HTMLDivElement>(null);
+	let option: echarts.EChartsOption = {
+		title: {
+			text: "Basic Radar Chart",
+			textStyle: {
+				color: "#a1a1a1",
+			},
+		},
+		legend: {
+			data: ["Allocated Budget", "Actual Spending"],
+			textStyle: {
+				color: "#a1a1a1",
+			},
+		},
+		radar: {
+			// shape: 'circle'
+			indicator: [
+				{ name: "Sales", max: 6500 },
+				{
+					name: "Administration",
+					max: 16000,
+				},
+				{
+					name: "Infomation Technology",
+					max: 30000,
+				},
+				{
+					name: "Customer Support",
+					max: 38000,
+				},
+				{
+					name: "Development",
+					max: 52000,
+				},
+				{
+					name: "Marketing",
+					max: 25000,
+				},
+			],
+		},
+		series: [
+			{
+				name: "Budget vs spending",
+				type: "radar",
+				data: [
+					{
+						value: [4200, 3000, 20000, 35000, 50000, 18000],
+						name: "Allocated Buget",
+					},
+					{
+						value: [5000, 14000, 28000, 26000, 42000, 21000],
+						name: "Actual Spending",
+					},
+				],
+			},
+		],
+	};
+
+	useEffect(() => {
+		const myChart: echarts.EChartsType = echarts.init(echartsRef.current as HTMLDivElement);
+		const { deleteEvent } = useEcharts(myChart, option);
+		return () => {
+			myChart && myChart.dispose();
+			deleteEvent && deleteEvent();
+		};
+	}, []);
+
+	return <div ref={echartsRef} className="content-box"></div>;
 };
 
 export default RadarChart;
